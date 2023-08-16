578e99acb4b2a7a20223395a550e648ad01daed3
Jacob Hummer <jcbhmr@outlook.com>
2023-08-16 01:12:29 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,7 @@ set -e
 apt update && apt install lsb-release wget software-properties-common gnupg -y -qq
 
 bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
+
+ln -s /usr/bin/clang-17 /usr/bin/clang
+ln -s /usr/bin/clang++-17 /usr/bin/clang++
+ln -s /usr/bin/lld-17 /usr/bin/lld
